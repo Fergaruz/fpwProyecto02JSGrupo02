79cee64ac1121d086e3a2e01618a406d620af7f3
Erika Anahi Quispe <erikaanahiquispe@email.com>
2026-09-11 15:20:41 -0300

    Declaración de constantes y función en JS. Ejercicio 05 - Erika Anahí Quispe

--- src/js/ejercicio05.js
+++ src/js/ejercicio05.js
@@ -1,2 +1,16 @@
 //Importacines
+
 //Constantes
+const formulario = document.querySelector("#formulario");
+const totalPreguntas = document.querySelector("#totalPreguntas");
+const respuestasCorrectas = document.querySelector("#respuestasCorrectas");
+const resultado = document.querySelector("#resultado");
+
+//Funciones
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    const preguntas = Number (totalPreguntas.value);
+    const correctas = Number (respuestasCorrectas.value);
+    const porcentaje = calcularPorcentaje (preguntas, correctas);
+    mostrarRendimiento(porcentaje);
+})
