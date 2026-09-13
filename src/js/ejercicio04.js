2148ade4c9cd42ffd662f34ceb0aac18070c2584
LeanDraw207 <rodriguezleandro207@gmail.com>
2026-09-12 21:58:03 -0300

    Proceso de suma de carácteres realizada

--- src/js/ejercicio04.js
+++ src/js/ejercicio04.js
@@ -5,24 +5,26 @@ let cadenaValida = true;
 if (cadenaIntroducida.length % 2 == 1){
     for(i=0; i<cadenaIntroducida.length ; i++ ){ //i vale cero, i es menor a la longitud de los caracteres?, i suma 1 a su valor
         if (i%2 === 0){ //Si la posición del carácter es par:
-            if (cadenaIntroducida[i] >= 0 && cadenaIntroducida[i] <= 5){ //Si el caracter está entre 0 y 5
+            if (Number(cadenaIntroducida[i]) >= 0 && Number(cadenaIntroducida[i]) <= 5){ //Si el caracter está entre 0 y 5
                 console.log("Está dentro del límite");
             } else{
                 cadenaValida = false;
-                console.log(cadenaValida + "Es mayor a 5")
+                console.log("El caracter no es válido")
             }
         } else {
             if (cadenaIntroducida[i]=== "?"){ //Si el carácter es un signo de pregunta:
-            console.log("Es un signo de pregunta");
+            console.log(Number(cadenaIntroducida[i-1]) + Number(cadenaIntroducida[i+1]));
             } else{
             cadenaValida = false;
-            console.log(cadenaValida + "No es un signo de pregunta");
+            console.log("El caracter no es válido");
             }
         };
     }
-    if (cadenaValida){
+    if (cadenaValida){ //Si la cadena es válida
         console.log("La cadena es valida");
-    };;
+    } else{
+        console.log("La cadena no es valida");
+    };
 } else {
     cadenaValida = false;
     console.log("La cadena no es valida");
