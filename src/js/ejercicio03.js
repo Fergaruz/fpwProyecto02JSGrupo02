36161388e6e561cabe3fa835c907ec87755fffa7
Andycap98 <140664285+Andycap98@users.noreply.github.com>
2026-09-14 10:49:04 -0300

    Comence el ejercicio 3

---
+++ src/js/ejercicio03.js
@@ -0,0 +1,5 @@
+//comence el ejercicio 3 !
+//Escribir un programa que solicite al usuario el ingreso del nombre de una ciudad y el nombre de un país. El programa debe concatenar ambas cadenas separándolas por una coma y un espacio en blanco (ej. "San Salvador de Jujuy, Argentina") y luego mostrar el resultado. Además, se debe realizar lo siguiente: mostrar la cantidad de caracteres de ambas cadenas por separado, compararlas e indicar cuál de las dos es más larga (o si son iguales).//
+const ciudad = prompt("Ingrese el nombre de una ciudad:");
+const pais = prompt("Ingrese el nombre de un país:");
+
