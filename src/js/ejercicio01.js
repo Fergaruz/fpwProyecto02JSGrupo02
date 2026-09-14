f50b6ee787a81337fade577d2e42a24bce67a0bc
Fergaruz <gaby2897@outlook.es>
2026-09-14 09:31:52 -0300

    Ruiz Fernando - Ejercicio 1 - Se han declarado las variables, se ha creado la funcion comparacion que compara ambas letras en la carpeta services, se ha importado la funcion comparacion al archivo ejercicio1.js y se ha creado el llamado a la funcion comparacion

--- src/js/ejercicio01.js
+++ src/js/ejercicio01.js
@@ -1,4 +1,12 @@
+//Importacón de la funcion
+
+import { comparacion } from "../services/serviciosEjercicio01.js";
+
 //Inicializacion de variables
 
 let a = prompt("Ingresa una letra para comparar");
 let b = prompt("Ingresa otra letra para comparar");
+
+//Llamado de la función
+
+comparacion(a,b);
