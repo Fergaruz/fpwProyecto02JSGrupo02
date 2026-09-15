9146a204ac49952afa073e51e6ccca5ef24cf97d
AstoriasYaman <alarconicolas04@gmail.com>
2026-09-14 22:25:54 -0300

    Nicolas Gonzalez - Ejercicio 2 realizado - Codigo agregado en los archivos ejercicio02.js, ejercicio02.html y serviciosEjercicio02.js

---
+++ src/services/serviciosEjercicio02.js
@@ -0,0 +1,6 @@
+//Creación y exportación de la función
+ 
+export const calcularAreaTriangulo = (base, altura) => {
+    const area = (base * altura) / 2;
+    alert(`El área del triángulo es: ${area}`);
+};
